731357c54148acdad5f8c939192f2723969ab7a1
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2021-12-19 21:27:54 -0500

    Add several utilities related to the servlet API and X.509 certs
    * Add KiwiServletRequests which provides mostly utils related to
      looking at the X509Certificates in a request
    * Add KiwiServletMocks which provides some Mockito-based utilities
      for mocking servlet requests. Currently, these are for cases when
      all you need is a certificate in a (mock) request.

---
+++ src/main/java/org/kiwiproject/beta/test/servlet/KiwiServletMocks.java
@@ -0,0 +1,122 @@
+package org.kiwiproject.beta.test.servlet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.common.annotations.Beta;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.kiwiproject.beta.servlet.KiwiServletRequests;
+import org.mockito.ArgumentMatcher;
+
+import javax.security.auth.x500.X500Principal;
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.security.cert.X509Certificate;
+
+/**
+ * Static utilities to create Mockito-based mocks for servlet API code.
+ */
+@UtilityClass
+@Slf4j
+@Beta
+public class KiwiServletMocks {
+
+    /**
+     * @implNote Don't inline the 'certificate' in the thenReturn() call on the mock request.
+     * For some reason that I have not fully investigated, Mockito gets really upset and throws
+     * a {@link org.mockito.exceptions.misusing.UnfinishedStubbingException}.
+     */
+    public static HttpServletRequest mockHttpServletRequestWithCertificate(String dn) {
+        var request = mock(HttpServletRequest.class);
+        var certificate = mockX509Certificate(dn);
+        when(request.getAttribute(KiwiServletRequests.X509_CERTIFICATE_ATTRIBUTE))
+                .thenReturn(new X509Certificate[]{certificate});
+        return request;
+    }
+
+    /**
+     * @implNote This is not strictly necessary since a Mockito mock will return null for methods that
+     * return a reference type if not provided any expectations. But, it makes test code more explicit
+     * about the intent of the code, so that's why this exists.
+     */
+    public static HttpServletRequest mockHttpServletRequestWithNoCertificate() {
+        var request = mock(HttpServletRequest.class);
+        when(request.getAttribute(KiwiServletRequests.X509_CERTIFICATE_ATTRIBUTE)).thenReturn(null);
+        return request;
+    }
+
+    /**
+     * @implNote Has to mock the {@link Principal} returned by {@link X509Certificate#getSubjectDN()} so
+     * this actually creates two mocks. Also, since {@link X509Certificate#getSubjectX500Principal()} returns
+     * an instance of the <em>final</em> class {@link X500Principal}, we can't mock it and instead a "real"
+     * instance of {@link X500Principal} having the given distinguished name is returned. Also see this
+     * https://softwareengineering.stackexchange.com/questions/173396/deprecated-vs-denigrated-in-javadoc
+     * regarding the getSubjectDN method being "denigrated".
+     */
+    public static X509Certificate mockX509Certificate(String dn) {
+        var cert = mock(X509Certificate.class);
+        var principal = mock(Principal.class);
+
+        when(cert.getSubjectDN()).thenReturn(principal);
+        when(principal.getName()).thenReturn(dn);
+
+        var x500Principal = new X500Principal(dn);
+        when(cert.getSubjectX500Principal()).thenReturn(x500Principal);
+
+        return cert;
+    }
+
+    /**
+     * Argument matcher that matches a certificate array containing exactly one {@link X509Certificate}
+     * having the given subject DN. Uses the {@link X509Certificate#getSubjectDN()} to obtain the
+     * {@link Principal} and then matches against {@link Principal#getName()}.
+     */
+    public static ArgumentMatcher<X509Certificate[]> matchesExpectedCertArrayBySubjectDN(String subjectDn) {
+
+        return certs -> {
+            assertThat(certs)
+                    .extracting(cert -> cert.getSubjectDN().getName())
+                    .containsExactly(subjectDn);
+
+            return true;
+        };
+    }
+
+    public static ArgumentMatcher<X509Certificate> matchesExpectedCertBySubjectDN(String subjectDn) {
+
+        return cert -> {
+            assertThat(cert.getSubjectDN().getName()).isEqualTo(subjectDn);
+
+            return true;
+        };
+    }
+
+    /**
+     * Argument matcher that matches a certificate array containing exactly one {@link X509Certificate}
+     * having an {@link X500Principal} with the given name. Uses {@link X509Certificate#getSubjectX500Principal()}
+     * and to obtain the {@link X500Principal} and then matches against {@link X500Principal#getName()}.
+     */
+    public static ArgumentMatcher<X509Certificate[]> matchesCertArrayByX500PrincipalName(String name) {
+
+        return object -> {
+            assertThat(object).isInstanceOf(X509Certificate[].class);
+
+            assertThat(object)
+                    .extracting(cert -> cert.getSubjectX500Principal().getName())
+                    .containsExactly(name);
+
+            return true;
+        };
+    }
+
+    public static ArgumentMatcher<X509Certificate> matchesExpectedCertByX500PrincipalName(String name) {
+
+        return cert -> {
+            assertThat(cert.getSubjectX500Principal().getName()).isEqualTo(name);
+
+            return true;
+        };
+    }
+}
---
+++ src/test/java/org/kiwiproject/beta/servlet/KiwiServletRequestsTest.java
@@ -0,0 +1,156 @@
+package org.kiwiproject.beta.servlet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.kiwiproject.beta.test.servlet.KiwiServletMocks;
+
+import java.security.cert.X509Certificate;
+
+@DisplayName("KiwiServletRequests")
+class KiwiServletRequestsTest {
+
+    @Nested
+    class HasCertificates {
+
+        @Test
+        void shouldBeTrue_WhenRequestContainsACertificate() {
+            var name = "CN=Bob,OU=Sales,O=ACME Inc,C=US";
+            var request = KiwiServletMocks.mockHttpServletRequestWithCertificate(name);
+            assertThat(KiwiServletRequests.hasCertificates(request)).isTrue();
+        }
+
+        @Test
+        void shouldBeFalse_WhenRequestDoesNotContainACertificate() {
+            var request = KiwiServletMocks.mockHttpServletRequestWithNoCertificate();
+            assertThat(KiwiServletRequests.hasCertificates(request)).isFalse();
+        }
+    }
+
+    @Nested
+    class GetCertificates {
+
+        @Test
+        void shouldReturnTheCerts_WhenRequestContainsACertificate() {
+            var name = "CN=Alice,OU=Marketing,O=ACME Inc,C=US";
+            var request = KiwiServletMocks.mockHttpServletRequestWithCertificate(name);
+            var certificates = KiwiServletRequests.getCertificates(request);
+
+            assertThat(certificates)
+                    .extracting(cert -> cert.getSubjectX500Principal().getName())
+                    .containsExactly(name);
+        }
+
+        @Test
+        void shouldReturnNull_WhenRequestDoesNotContainACertificate() {
+            var request = KiwiServletMocks.mockHttpServletRequestWithNoCertificate();
+            var certificates = KiwiServletRequests.getCertificates(request);
+            assertThat(certificates).isNull();
+        }
+    }
+
+    @Nested
+    class GetCertificatesOrEmpty {
+
+        @Test
+        void shouldReturnTheCerts_WhenRequestContainsACertificate() {
+            var name = "CN=Diane,OU=Engineering,O=ACME Inc,C=US";
+            var request = KiwiServletMocks.mockHttpServletRequestWithCertificate(name);
+            var certificates = KiwiServletRequests.getCertificatesOrEmpty(request);
+
+            assertThat(certificates).isPresent();
+
+            assertThat(certificates.get())
+                    .extracting(cert -> cert.getSubjectX500Principal().getName())
+                    .containsExactly(name);
+        }
+
+        @Test
+        void shouldReturnNull_WhenRequestDoesNotContainACertificate() {
+            var request = KiwiServletMocks.mockHttpServletRequestWithNoCertificate();
+            var certificates = KiwiServletRequests.getCertificatesOrEmpty(request);
+            assertThat(certificates).isEmpty();
+        }
+    }
+
+    @Nested
+    class HasCertificateIn {
+
+        @Test
+        void shouldBeTrue_WhenCertArrayContainsCert() {
+            var cert = mock(X509Certificate.class);
+            assertThat(KiwiServletRequests.hasCertificateIn(new X509Certificate[]{cert})).isTrue();
+        }
+
+        @Test
+        void shouldBeFalse_WhenCertArrayIsNull() {
+            assertThat(KiwiServletRequests.hasCertificateIn(null)).isFalse();
+        }
+
+        @Test
+        void shouldBeFalse_WhenFirstCertIsNull() {
+            assertThat(KiwiServletRequests.hasCertificateIn(new X509Certificate[]{null})).isFalse();
+        }
+    }
+
+    @Nested
+    class DoesNotHaveCertificateIn {
+
+        @Test
+        void shouldBeTrue_WhenCertArrayIsNull() {
+            assertThat(KiwiServletRequests.doesNotHaveCertificateIn(null)).isTrue();
+        }
+
+        @Test
+        void shouldBeTrue_WhenFirstCertArrayIsNull() {
+            assertThat(KiwiServletRequests.doesNotHaveCertificateIn(new X509Certificate[]{null})).isTrue();
+        }
+
+        @Test
+        void shouldBeFalse_WhenCertArrayContainsCert() {
+            var cert = mock(X509Certificate.class);
+            assertThat(KiwiServletRequests.doesNotHaveCertificateIn(new X509Certificate[]{cert})).isFalse();
+        }
+    }
+
+    @Nested
+    class FirstCertificate {
+
+        @Test
+        void shouldReturnFirstCertificate() {
+            var cert = mock(X509Certificate.class);
+            var certs = new X509Certificate[]{cert};
+            assertThat(KiwiServletRequests.firstCertificate(certs)).isSameAs(cert);
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void shouldThrowIllegalState_WhenGivenNullOrEmptyCertArray(X509Certificate[] certs) {
+            assertThatIllegalStateException().isThrownBy(() -> KiwiServletRequests.firstCertificate(certs));
+        }
+    }
+
+    @Nested
+    class FirstCertificateOrEmpty {
+
+        @Test
+        void shouldReturnFirstCertificate() {
+            var cert = mock(X509Certificate.class);
+            var certs = new X509Certificate[]{cert};
+            assertThat(KiwiServletRequests.firstCertificateOrEmpty(certs)).contains(cert);
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void shouldReturnEmpty_WhenGivenNullOrEmptyCertArray(X509Certificate[] certs) {
+            assertThat(KiwiServletRequests.firstCertificateOrEmpty(certs)).isEmpty();
+        }
+    }
+
+}
