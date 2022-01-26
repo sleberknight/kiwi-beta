8cbf122ad0de61b9c7f16595d01bccc9368acec1
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-01-26 08:59:31 -0500

    Add guard around log method to avoid array copy unless it is necessary

--- src/main/kotlin/org/kiwiproject/beta/slf4j/KiwiSlf4jExtensions.kt
+++ src/main/kotlin/org/kiwiproject/beta/slf4j/KiwiSlf4jExtensions.kt
@@ -13,8 +13,15 @@ fun Logger.log(level: Level, format: String, arg: Any) =
 fun Logger.log(level: Level, format: String, arg1: Any, arg2: Any) =
     KiwiSlf4j.log(this, level, format, arg1, arg2)
 
-fun Logger.log(level: Level, format: String, vararg arguments: Any) =
-    KiwiSlf4j.log(this, level, format, *arguments)
+// Add guard around log to avoid an array copy by the spread operator unless actually necessary.
+// I don't know how else to "fix" the use of the spread operator here.
+// See https://detekt.github.io/detekt/performance.html#spreadoperator for more information
+@Suppress("SpreadOperator")
+fun Logger.log(level: Level, format: String, vararg arguments: Any) {
+    if (isEnabled(level)) {
+        KiwiSlf4j.log(this, level, format, *arguments)
+    }
+}
 
 fun Logger.log(level: Level, message: String, t: Throwable) =
     KiwiSlf4j.log(this, level, message, t)
