6541fc765b1604e2aadc894be3a6122a0e68caab
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-11-05 21:16:29 -0400

    Add additional camelToSnakeCase methods
    * camelToSnakeCase (requires non-blank input)
    * camelToSnakeCaseOrEmpty

--- src/main/java/org/kiwiproject/beta/base/KiwiStrings2.java
+++ src/main/java/org/kiwiproject/beta/base/KiwiStrings2.java
@@ -4,10 +4,10 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 
 import com.google.common.annotations.Beta;
 import com.google.common.base.CaseFormat;
-
+import lombok.experimental.UtilityClass;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-import lombok.experimental.UtilityClass;
+import java.util.Optional;
 
 /**
  * Utilities related to strings.
@@ -20,11 +20,39 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class KiwiStrings2 {
 
-    public static String camelToSnakeCaseOrNull(@Nullable String camelCaseValue) {
-        if (isBlank(camelCaseValue)) {
+    /**
+     * Convert a camelCase value to snake_case.
+     *
+     * @param value the camelCase value, must not be blank
+     * @return the converted snake_case value
+     * @throws IllegalArgumentException if value is blank
+     */
+    public static String camelToSnakeCase(String value) {
+        return camelToSnakeCaseOrEmpty(value)
+                .orElseThrow(() -> new IllegalArgumentException("value must not be blank"));
+    }
+
+    /**
+     * Convert a camelCase value to snake_case.
+     *
+     * @param value the camelCase value
+     * @return Optional containing the converted snake_case value, or an empty Optional
+     */
+    public static Optional<String> camelToSnakeCaseOrEmpty(@Nullable String value) {
+        return Optional.ofNullable(camelToSnakeCaseOrNull(value));
+    }
+
+    /**
+     * Convert a camelCase value to snake_case.
+     *
+     * @param value the camelCase value
+     * @return the converted snake_case value, or null if the input value is blank
+     */
+    public static String camelToSnakeCaseOrNull(@Nullable String value) {
+        if (isBlank(value)) {
             return null;
         }
 
-        return CaseFormat.LOWER_CAMEL.converterTo(CaseFormat.LOWER_UNDERSCORE).convert(camelCaseValue);
+        return CaseFormat.LOWER_CAMEL.converterTo(CaseFormat.LOWER_UNDERSCORE).convert(value);
     }
 }
--- src/test/java/org/kiwiproject/beta/base/KiwiStrings2Test.java
+++ src/test/java/org/kiwiproject/beta/base/KiwiStrings2Test.java
@@ -1,6 +1,7 @@
 package org.kiwiproject.beta.base;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -12,26 +13,82 @@ import org.kiwiproject.test.junit.jupiter.params.provider.AsciiOnlyBlankStringSo
 public class KiwiStrings2Test {
 
     @Nested
+    class CamelToSnakeCase {
+
+        @ParameterizedTest
+        @AsciiOnlyBlankStringSource
+        void shouldNotAllowBlankInput(String value) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> KiwiStrings2.camelToSnakeCase(value))
+                    .withMessage("value must not be blank");
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "id, id",
+                "source, source",
+                "tagsJson, tags_json",
+                "createdAt, created_at",
+                "createdById, created_by_id",
+                "expirationDate, expiration_date",
+                "MarkedForRemoval, marked_for_removal",
+                "DeletedAt, deleted_at",
+                "created_at, created_at",
+                "updated_at, updated_at",
+        })
+        void shouldConvertTo_SnakeCase(String input, String expected) {
+            assertThat(KiwiStrings2.camelToSnakeCase(input)).isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    class CamelToSnakeCaseOrEmpty {
+
+        @ParameterizedTest
+        @AsciiOnlyBlankStringSource
+        void shouldReturnEmpty_GivenBlankInput(String value) {
+            assertThat(KiwiStrings2.camelToSnakeCaseOrEmpty(value)).isEmpty();
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "id, id",
+                "source, source",
+                "tagsJson, tags_json",
+                "createdAt, created_at",
+                "createdById, created_by_id",
+                "expirationDate, expiration_date",
+                "MarkedForRemoval, marked_for_removal",
+                "DeletedAt, deleted_at",
+                "created_at, created_at",
+                "updated_at, updated_at",
+        })
+        void shouldConvertTo_SnakeCase(String input, String expected) {
+            assertThat(KiwiStrings2.camelToSnakeCaseOrEmpty(input)).contains(expected);
+        }
+    }
+
+    @Nested
     class CamelToSnakeCaseOrNull {
 
         @ParameterizedTest
         @AsciiOnlyBlankStringSource
-        void shouldReturnNull_GivenBlnkInput(String value) {
+        void shouldReturnNull_GivenBlankInput(String value) {
             assertThat(KiwiStrings2.camelToSnakeCaseOrNull(value)).isNull();
         }
 
         @ParameterizedTest
         @CsvSource({
-            "id, id",
-            "source, source",
-            "tagsJson, tags_json",
-            "createdAt, created_at",
-            "createdById, created_by_id",
-            "expirationDate, expiration_date",
-            "MarkedForRemoval, marked_for_removal",
-            "DeletedAt, deleted_at",
-            "created_at, created_at",
-            "updated_at, updated_at",
+                "id, id",
+                "source, source",
+                "tagsJson, tags_json",
+                "createdAt, created_at",
+                "createdById, created_by_id",
+                "expirationDate, expiration_date",
+                "MarkedForRemoval, marked_for_removal",
+                "DeletedAt, deleted_at",
+                "created_at, created_at",
+                "updated_at, updated_at",
         })
         void shouldConvertTo_SnakeCase(String input, String expected) {
             assertThat(KiwiStrings2.camelToSnakeCaseOrNull(input)).isEqualTo(expected);
