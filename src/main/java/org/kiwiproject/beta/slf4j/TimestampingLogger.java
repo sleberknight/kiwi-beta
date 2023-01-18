b34775ad1482348f6d8cb8732be17df1bb6d6234
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-01-18 07:22:28 -0500

    Add TimestampingLogger - a Logger that logs elapsed time between logs

---
+++ src/main/java/org/kiwiproject/beta/slf4j/TimestampingLogger.java
@@ -0,0 +1,147 @@
+package org.kiwiproject.beta.slf4j;
+
+import static org.kiwiproject.base.KiwiPreconditions.requireNotNull;
+
+import com.google.common.annotations.Beta;
+import org.kiwiproject.base.KiwiStrings;
+import org.slf4j.Logger;
+import org.slf4j.event.Level;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import java.time.Duration;
+
+/**
+ * A simple way to log timing information about a repeated operation.
+ * <p>
+ * This is intended to be used for a single logical operation which contains multiple steps, or for the same operation
+ * repeated over a collection. For example, an order operation with separate steps to find a user, create a new order
+ * linked to the user, and insert the order to a database. Or, some kind of data migration that loops over records in
+ * a source database table and writes to a new target database table.
+ * <p>
+ * <em>Currently, this is intended only to be used within a single thread.</em>
+ */
+@NotThreadSafe
+@Beta
+public class TimestampingLogger {
+
+    @SuppressWarnings("NonConstantLogger")
+    private final Logger logger;
+    private long previousTimestamp;
+
+    /**
+     * Create a new instance using the given {@link Logger}.
+     */
+    public TimestampingLogger(Logger logger) {
+        this.logger = requireNotNull(logger);
+    }
+
+    /**
+     * Logs a message and elapsed time message at TRACE level.
+     *
+     * @param message the message or message template
+     * @param args    the arguments to the message template, if any
+     * @see #logElapsed(Level, String, Object...)
+     */
+    public void traceLogElapsed(String message, Object... args) {
+        logElapsed(Level.TRACE, message, args);
+    }
+
+    /**
+     * Logs a message and elapsed time message at DEBUG level.
+     *
+     * @param message the message or message template
+     * @param args    the arguments to the message template, if any
+     * @see #logElapsed(Level, String, Object...)
+     */
+    public void debugLogElapsed(String message, Object... args) {
+        logElapsed(Level.DEBUG, message, args);
+    }
+
+    /**
+     * Logs the given message, and then logs the elapsed time since the previous log.
+     * This results in two separate log messages.
+     *
+     * @param level   the level at which to log the message and elapsed time message
+     * @param message the message or message template
+     * @param args    the arguments to the message template, if any
+     */
+    public void logElapsed(Level level, String message, Object... args) {
+        if (KiwiSlf4j.isEnabled(logger, level)) {
+            var now = System.nanoTime();
+            KiwiSlf4j.log(logger, level, message, args);
+            logElapsedSincePreviousTimestamp(logger, level, now, previousTimestamp);
+            previousTimestamp = now;
+        }
+    }
+
+    private static void logElapsedSincePreviousTimestamp(Logger logger,
+                                                         Level level,
+                                                         long now,
+                                                         long previousTimestamp) {
+        if (previousTimestamp > 0) {
+            var diffInNanos = now - previousTimestamp;
+            KiwiSlf4j.log(logger, level,
+                    " -- Time spent (since previous log): {} nanoseconds / {} millis",
+                    diffInNanos, Duration.ofNanos(diffInNanos).toMillis());
+        } else {
+            KiwiSlf4j.log(logger, level,
+                    " -- Time spent (since previous log): N/A (no previous timestamp)");
+        }
+    }
+
+    /**
+     * Logs a message including elapsed time message at TRACE level.
+     *
+     * @param message the message or message template
+     * @param args    the arguments to the message template, if any
+     * @see #logEmbeddingElapsed(Level, String, Object...)
+     */
+    public void traceLogEmbeddingElapsed(String message, Object... args) {
+        logEmbeddingElapsed(Level.TRACE, message, args);
+    }
+
+    /**
+     * Logs a message including elapsed time message at DEBUG level.
+     *
+     * @param message the message or message template
+     * @param args    the arguments to the message template, if any
+     * @see #logEmbeddingElapsed(Level, String, Object...)
+     */
+    public void debugLogEmbeddingElapsed(String message, Object... args) {
+        logEmbeddingElapsed(Level.DEBUG, message, args);
+    }
+
+    /**
+     * Logs the given message along with the elapsed time since the previous log.
+     * This results in a single log message containing the original message followed by the elapsed time message.
+     *
+     * @param level   the level at which to log the message and elapsed time message
+     * @param message the message or message template
+     * @param args    the arguments to the message template, if any
+     */
+    public void logEmbeddingElapsed(Level level, String message, Object... args) {
+        if (KiwiSlf4j.isEnabled(logger, level)) {
+            var now = System.nanoTime();
+            var formattedMessage = KiwiStrings.f(message, args);
+            logEmbeddedElapsedSincePreviousTimestamp(logger, level, formattedMessage, now, previousTimestamp);
+            previousTimestamp = now;
+        }
+    }
+
+    private static void logEmbeddedElapsedSincePreviousTimestamp(Logger logger,
+                                                                 Level level,
+                                                                 String formattedMessage,
+                                                                 long now,
+                                                                 long previousTimestamp) {
+        if (previousTimestamp > 0) {
+            var diffInNanos = now - previousTimestamp;
+            KiwiSlf4j.log(logger, level,
+                    "{} -- Time spent (since previous log): {} nanoseconds / {} millis",
+                    formattedMessage, diffInNanos, Duration.ofNanos(diffInNanos).toMillis());
+        } else {
+            KiwiSlf4j.log(logger, level,
+                    "{} -- Time spent (since previous log): N/A (no previous timestamp)",
+                    formattedMessage);
+        }
+    }
+}
---
+++ src/test/java/org/kiwiproject/beta/slf4j/TimestampingLoggerTest.java
@@ -0,0 +1,221 @@
+package org.kiwiproject.beta.slf4j;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.Index.atIndex;
+import static org.kiwiproject.collect.KiwiLists.fifth;
+import static org.kiwiproject.collect.KiwiLists.first;
+import static org.kiwiproject.collect.KiwiLists.fourth;
+import static org.kiwiproject.collect.KiwiLists.second;
+import static org.kiwiproject.collect.KiwiLists.subListExcludingFirst;
+import static org.kiwiproject.collect.KiwiLists.third;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.kiwiproject.beta.test.logback.InMemoryAppender;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@DisplayName("TimestampingLogger")
+class TimestampingLoggerTest {
+
+    private static Logger logbackLogger;
+    private static InMemoryAppender appender;
+
+    private TimestampingLogger timestampingLogger;
+    private List<String> messages;
+
+    @BeforeAll
+    static void beforeAll() {
+        logbackLogger = (Logger) LoggerFactory.getLogger(TimestampingLoggerTest.class);
+        resetLevelToTrace(logbackLogger);
+
+        var context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        appender = new InMemoryAppender();
+        appender.setContext(context);
+        appender.start();
+
+        logbackLogger.addAppender(appender);
+    }
+
+    @BeforeEach
+    void setUp() {
+        timestampingLogger = new TimestampingLogger(logbackLogger);
+
+        messages = List.of("At time 0", "At time 1", "At time 2", "At time 3", "At time 4");
+    }
+
+    @AfterEach
+    void tearDown() {
+        appender.clearEvents();
+        resetLevelToTrace(logbackLogger);
+    }
+
+    private static void resetLevelToTrace(Logger logger) {
+        logger.setLevel(ch.qos.logback.classic.Level.TRACE);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        appender.stop();
+        logbackLogger.detachAppender(appender);
+    }
+
+    @Test
+    void shouldLogElapsedTimeWithArguments() {
+        timestampingLogger.logElapsed(Level.DEBUG, "{} created with id {}", "User", 42);
+        timestampingLogger.logElapsed(Level.DEBUG, "{} updated with id {}", "Order", 336);
+
+        List<String> eventMessages = appender.getOrderedEventMessages();
+
+        assertThat(eventMessages).hasSize(4);
+        assertThat(first(eventMessages)).isEqualTo("User created with id 42");
+        assertThat(second(eventMessages)).isEqualTo(" -- Time spent (since previous log): N/A (no previous timestamp)");
+        assertThat(third(eventMessages)).isEqualTo("Order updated with id 336");
+        assertThat(fourth(eventMessages)).startsWith(" -- Time spent (since previous log): ")
+                .contains(" nanoseconds /")
+                .endsWith(" millis");
+    }
+
+    @Test
+    void shouldNotLogElapsedTimeWhenLevelIsInactive() {
+        logbackLogger.setLevel(ch.qos.logback.classic.Level.WARN);
+
+        timestampingLogger.logElapsed(Level.INFO, "Should not see this!");
+        assertThat(appender.getOrderedEventMessages()).isEmpty();
+    }
+
+    @Test
+    void shouldTraceLogElapsedTime() {
+        messages.forEach(message -> timestampingLogger.traceLogElapsed(message));
+        assertElapsedEventMessages(Level.TRACE);
+    }
+
+    @Test
+    void shouldDebugLogElapsedTime() {
+        messages.forEach(message -> timestampingLogger.debugLogElapsed(message));
+        assertElapsedEventMessages(Level.DEBUG);
+    }
+
+    @Test
+    void shouldLogElapsedTime() {
+        messages.forEach(message -> timestampingLogger.logElapsed(Level.INFO, message));
+        assertElapsedEventMessages(Level.INFO);
+    }
+
+    private void assertElapsedEventMessages(Level expectedLevel) {
+        List<ILoggingEvent> orderedEvents = appender.getOrderedEvents();
+        var expectedLogbackLevel = ch.qos.logback.classic.Level.convertAnSLF4JLevel(expectedLevel);
+        assertThat(orderedEvents)
+                .describedAs("All events should have level %s", expectedLevel)
+                .isNotEmpty()
+                .allMatch(event -> event.getLevel() == expectedLogbackLevel);
+
+        List<String> eventMessages = appender.getOrderedEventMessages();
+        assertThat(eventMessages)
+                .describedAs("Should have 10 messages; even indices should have actual log messages")
+                .hasSize(10)
+                .contains(first(messages), atIndex(0))
+                .contains(second(messages), atIndex(2))
+                .contains(third(messages), atIndex(4))
+                .contains(fourth(messages), atIndex(6))
+                .contains(fifth(messages), atIndex(8));
+
+        var timeSpentMessages = eventMessages.stream()
+                .filter(eventMessage -> eventMessage.startsWith(" -- Time spent"))
+                .collect(toList());
+
+        assertThat(timeSpentMessages)
+                .describedAs("Should have five messages with elapsed time")
+                .hasSize(5);
+
+        assertThat(first(timeSpentMessages))
+                .describedAs("First elapsed time message should say there is no previous timestamp")
+                .isEqualTo(" -- Time spent (since previous log): N/A (no previous timestamp)");
+
+        subListExcludingFirst(timeSpentMessages).forEach(timeSpentMessage ->
+                assertThat(timeSpentMessage)
+                        .describedAs("Rest of elapsed time messages should include nanoseconds and millis")
+                        .startsWith(" -- Time spent (since previous log): ")
+                        .contains(" nanoseconds / ")
+                        .endsWith(" millis"));
+    }
+
+    @Test
+    void shouldLogEmbeddingElapsedTimeWithArguments() {
+        timestampingLogger.logEmbeddingElapsed(Level.TRACE, "{} created with id {}", "User", 24);
+        timestampingLogger.logEmbeddingElapsed(Level.TRACE, "{} updated with id {}", "Order", 84);
+
+        List<String> eventMessages = appender.getOrderedEventMessages();
+
+        assertThat(eventMessages).hasSize(2);
+        assertThat(first(eventMessages))
+                .isEqualTo("User created with id 24 -- Time spent (since previous log): N/A (no previous timestamp)");
+        assertThat(second(eventMessages))
+                .startsWith("Order updated with id 84 -- Time spent (since previous log): ")
+                .contains(" nanoseconds / ")
+                .endsWith(" millis");
+    }
+
+    @Test
+    void shouldNotLogEmbeddingElapsedTimeWhenLevelIsInactive() {
+        logbackLogger.setLevel(ch.qos.logback.classic.Level.ERROR);
+
+        timestampingLogger.logEmbeddingElapsed(Level.WARN, "Should not see this!");
+        assertThat(appender.getOrderedEventMessages()).isEmpty();
+    }
+
+    @Test
+    void shouldTraceLogEmbeddingElapsedTime() {
+        messages.forEach(message -> timestampingLogger.traceLogEmbeddingElapsed(message));
+        assertEmbeddedElapsedEventMessages(Level.TRACE);
+    }
+
+    @Test
+    void shouldDebugLogEmbeddingElapsedTime() {
+        messages.forEach(message -> timestampingLogger.debugLogEmbeddingElapsed(message));
+        assertEmbeddedElapsedEventMessages(Level.DEBUG);
+    }
+
+    @Test
+    void shouldLogEmbeddingElapsedTime() {
+        messages.forEach(message -> timestampingLogger.logEmbeddingElapsed(Level.WARN, message));
+        assertEmbeddedElapsedEventMessages(Level.WARN);
+    }
+
+    private void assertEmbeddedElapsedEventMessages(Level expectedLevel) {
+        List<ILoggingEvent> orderedEvents = appender.getOrderedEvents();
+        var expectedLogbackLevel = ch.qos.logback.classic.Level.convertAnSLF4JLevel(expectedLevel);
+        assertThat(orderedEvents)
+                .describedAs("All events should have level %s", expectedLevel)
+                .isNotEmpty()
+                .allMatch(event -> event.getLevel() == expectedLogbackLevel);
+
+        List<String> eventMessages = appender.getOrderedEventMessages();
+        assertThat(eventMessages)
+                .describedAs("Should have 5 messages")
+                .hasSize(5);
+
+        assertThat(first(eventMessages))
+                .describedAs("First embedded elapsed time message should say there is no previous timestamp")
+                .isEqualTo("At time 0 -- Time spent (since previous log): N/A (no previous timestamp)");
+
+        IntStream.range(1, 4).forEach(i ->
+                assertThat(eventMessages.get(i))
+                        .describedAs("Rest of embedded elapsed time messages should include nanoseconds and millis")
+                        .startsWith("At time " + i + " -- Time spent (since previous log): ")
+                        .contains(" nanoseconds / ")
+                        .endsWith(" millis"));
+    }
+}
