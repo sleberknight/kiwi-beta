567bc13978357fca22295d0bc200402bd4434aca
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-03-21 22:25:15 +0000

    Add KiwiFutures
    Contains two methods that replicate the functionality of several methods
    that were added to Future in JDK 19, namely resultNow and exceptionNow.
    Here, they are obviously not added to Future, since it's a JDK interface that
    we cannot change. Instead they are implemented as static utility methods
    that accept a Future argument.

--- src/test/java/org/kiwiproject/beta/concurrent/KiwiFuturesTest.java
+++ src/test/java/org/kiwiproject/beta/concurrent/KiwiFuturesTest.java
@@ -1,8 +1,9 @@
 package org.kiwiproject.beta.concurrent;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
-
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.kiwiproject.base.DefaultEnvironment;
@@ -10,12 +11,25 @@ import org.kiwiproject.base.DefaultEnvironment;
 import java.io.IOException;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 class KiwiFuturesTest {
 
+    @AfterEach
+    void tearDown() {
+        KiwiFutures.resetInterruptedExceptionHandler();
+    }
+
     @Nested
     class ResultNow {
+
+        @Test
+        void shouldThrowIllegalArgument_WhenGivenNullFuture() {
+            assertThatIllegalArgumentException().isThrownBy(() -> KiwiFutures.resultNow(null));
+        }
 
         @Test
         void shouldReturnResult_WhenFutureIsDoneWithResult() {
@@ -23,6 +37,20 @@ class KiwiFuturesTest {
             var future = CompletableFuture.completedFuture(value);
 
             assertThat(KiwiFutures.resultNow(future)).isEqualTo(value);
+        }
+
+        @Test
+        void shouldThrowIllegalState_WhenInterruptedExceptionOccurs() throws Exception {
+            KiwiFutures.interruptedExceptionHandler = e -> {
+                return new IllegalStateException("I was rudely interrupted", e);
+            };
+
+            Future<Long> future = new AlwaysInterruptingFuture();
+
+            assertThatIllegalStateException()
+                    .isThrownBy(() -> KiwiFutures.resultNow(future))
+                    .withMessage("I was rudely interrupted")
+                    .withCauseExactlyInstanceOf(InterruptedException.class);
         }
 
         @Test
@@ -69,11 +97,30 @@ class KiwiFuturesTest {
     class ExceptionNow {
 
         @Test
+        void shouldThrowIllegalArgument_WhenGivenNullFuture() {
+            assertThatIllegalArgumentException().isThrownBy(() -> KiwiFutures.exceptionNow(null));
+        }
+
+        @Test
         void shouldReturnException_WhenIsDoneAndCompletesExceptionally() {
             var exception = new IOException("I/O error - device 456");
             var future = CompletableFuture.failedFuture(exception);
 
             assertThat(KiwiFutures.exceptionNow(future)).isSameAs(exception);
+        }
+
+        @Test
+        void shouldThrowIllegalState_WhenInterruptedExceptionOccurs() throws Exception {
+            KiwiFutures.interruptedExceptionHandler = e -> {
+                return new IllegalStateException("Someone interrupted me!", e);
+            };
+
+            Future<Long> future = new AlwaysInterruptingFuture();
+
+            assertThatIllegalStateException()
+                    .isThrownBy(() -> KiwiFutures.exceptionNow(future))
+                    .withMessage("Someone interrupted me!")
+                    .withCauseExactlyInstanceOf(InterruptedException.class);
         }
 
         @Test
@@ -113,6 +160,63 @@ class KiwiFuturesTest {
                     .isThrownBy(() -> KiwiFutures.exceptionNow(future))
                     .withMessage("Task completed with a result")
                     .withNoCause();
+        }
+    }
+
+    @Nested
+    class IsNotDone {
+
+        @Test
+        void shouldBeTrue_WhenFutureIsNotDone() {
+            var future = CompletableFuture.supplyAsync(() -> {
+                new DefaultEnvironment().sleepQuietly(10, TimeUnit.SECONDS);
+                return 24 * 42;
+            });
+
+            assertThat(KiwiFutures.isNotDone(future)).isTrue();
+
+            var wasCancelled = future.cancel(true);
+            assertThat(wasCancelled).isTrue();
+        }
+
+        @Test
+        void shouldBeFalse_WhenFutureIsDone() {
+            var future = CompletableFuture.completedFuture("foo");
+
+            assertThat(KiwiFutures.isNotDone(future)).isFalse();
+        }
+    }
+
+    /**
+     * @implNote Using a "real mock" instead of Mockito here because it is simple to do so, and avoids incurring
+     * the "startup time" of Mockito.
+     */
+    static class AlwaysInterruptingFuture implements Future<Long> {
+
+        @Override
+        public boolean cancel(boolean mayInterruptIfRunning) {
+            return false;
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return false;
+        }
+
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+
+        @Override
+        public Long get() throws InterruptedException, ExecutionException {
+            throw new InterruptedException("simuated interruption");
+        }
+
+        @Override
+        public Long get(long timeout, TimeUnit unit)
+                throws InterruptedException, ExecutionException, TimeoutException {
+                    throw new InterruptedException("simuated interruption (with timeout)");
         }
     }
 }
