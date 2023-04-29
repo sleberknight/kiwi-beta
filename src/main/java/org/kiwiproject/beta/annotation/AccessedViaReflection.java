b5091fd9cb4b1ec0b0f160d16b1df49fd552b63d
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-04-29 18:13:41 -0400

    Allow AccessedViaReflection to apply to "Element.TYPE" (#247)
    * Allow AccessedViaReflection to apply to "Element.TYPE"
    
    * AccessedViaReflection should be permitted on classes (types)
    * Add AccessedViaReflection annotation in KiwiReflection2Test (due
      to some false positives found by CodeQL)
    
    Closes #246
    
    * Add javadoc explaining when this annotation might be useful on a type

--- src/main/java/org/kiwiproject/beta/annotation/AccessedViaReflection.java
+++ src/main/java/org/kiwiproject/beta/annotation/AccessedViaReflection.java
@@ -3,6 +3,7 @@ package org.kiwiproject.beta.annotation;
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 
 import com.google.common.annotations.Beta;
 
@@ -16,13 +17,17 @@ import java.lang.annotation.Target;
  * When placed on a method or constructor, indicates that the member is called via
  * reflection, and is this used and should not be considered for removal. When placed
  * on a field, indicates that the field is accessed via reflection to get or set it.
+ * When placed on a type (class, interface, annotation, enum, or record), indicates that
+ * the type may be loaded via reflection using {@link Class#forName(String)} or a similar
+ * mechanism. Placing on a type can also mean that the type and/or some or all of its
+ * members may be accessed reflectively.
  * <p>
  * When this annotation is present, you might also consider adding {@code @SuppressWarnings("unused")}.
  * This is necessary since {@link SuppressWarnings} is not {@link Inherited} so we cannot
  * include it in this annotation.
  */
 @Documented
-@Target({METHOD, CONSTRUCTOR, FIELD})
+@Target({METHOD, CONSTRUCTOR, FIELD, TYPE})
 @Retention(RetentionPolicy.SOURCE)
 @Beta
 public @interface AccessedViaReflection {
