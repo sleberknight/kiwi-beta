9daea65fdf826400979fa762d3ea191dab06ab39
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-02-15 13:57:31 -0500

    Add CountingPredicate (#385)

---
+++ src/main/java/org/kiwiproject/beta/util/function/CountingPredicate.java
@@ -0,0 +1,70 @@
+package org.kiwiproject.beta.util.function;
+
+import static org.kiwiproject.base.KiwiPreconditions.requireNotNull;
+
+import com.google.common.annotations.Beta;
+import org.checkerframework.checker.index.qual.NonNegative;
+
+import java.util.function.Predicate;
+
+/**
+ * A Predicate that wraps a Predicate and tracks the number of
+ * true and false results.
+ * <p>
+ * This should be used only on one stream, unless you want to
+ * continue to accumulate the number of true and false results
+ * over multiple streams.
+ * <p>
+ * Exceptions thrown by the wrapped Predicate are passed through.
+ */
+@Beta
+public class CountingPredicate<T> implements Predicate<T> {
+
+    private final Predicate<T> predicate;
+    private long trueCount;
+    private long falseCount;
+
+    /**
+     * Create a new instance that wraps {@code predicate} in order to
+     * count the number of true and false results.
+     *
+     * @param predicate
+     */
+    public CountingPredicate(Predicate<T> predicate) {
+        this.predicate = requireNotNull(predicate);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean test(T t) {
+        var result = predicate.test(t);
+        if (result) {
+            ++trueCount;
+        } else {
+            ++falseCount;
+        }
+        return result;
+    }
+
+    /**
+     * Return the number of times this predicate has gotten a {@code true} result.
+     *
+     * @return the total number of true results
+     */
+    @NonNegative
+    public long trueCount() {
+        return trueCount;
+    }
+
+    /**
+     * Return the number of times this predicate has gotten a {@code false} result.
+     *
+     * @return the total number of false results
+     */
+    @NonNegative
+    public long falseCount() {
+        return falseCount;
+    }
+}
---
+++ src/test/java/org/kiwiproject/beta/util/function/CountingPredicateTest.java
@@ -0,0 +1,82 @@
+package org.kiwiproject.beta.util.function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+@DisplayName("CountingPredicate")
+class CountingPredicateTest {
+
+    @Test
+    void shouldRequireNonNullPredicate() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new CountingPredicate<>(null));
+    }
+
+    @Test
+    void shouldHaveZeroInitialCounts() {
+        var isOdd = new CountingPredicate<Integer>(value -> value % 2 != 0);
+
+        assertThat(isOdd.trueCount()).isZero();
+        assertThat(isOdd.falseCount()).isZero();
+    }
+
+    @ParameterizedTest
+    @MethodSource("collectionsOfIntFactory")
+    void shouldTrackCountsInCollections(Collection<Integer> collection) {
+        var isEven = new CountingPredicate<Integer>(value -> value % 2 == 0);
+
+        collection.stream().filter(isEven).toList();
+
+        assertThat(isEven.trueCount()).isEqualTo(4);
+        assertThat(isEven.falseCount()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldTrackCountsInArrays() {
+        var isOdd = new CountingPredicate<Integer>(value -> value % 2 != 0);
+
+        Integer[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        Arrays.stream(numbers).filter(isOdd).toList();
+
+        assertThat(isOdd.trueCount()).isEqualTo(5);
+        assertThat(isOdd.falseCount()).isEqualTo(4);
+    }
+
+    @Test
+    void shouldAccumulateCounts_WhenPredicateisUsedMultipleTimes() {
+        var ints1 = List.of(1, 2, 3, 4);
+        var ints2 = Set.of(5, 6, 7, 8);
+        var ints3 = List.of(9);
+        var ints4 = Set.of(10, 11, 12);
+        var ints5 = List.of(15);
+
+        var isEven = new CountingPredicate<Integer>(value -> value % 2 == 0);
+
+        ints1.stream().filter(isEven).toList();
+        ints2.stream().filter(isEven).toList();
+        ints3.stream().filter(isEven).toList();
+        ints4.stream().filter(isEven).toList();
+        ints5.stream().filter(isEven).toList();
+
+        assertThat(isEven.trueCount()).isEqualTo(6);
+        assertThat(isEven.falseCount()).isEqualTo(7);
+    }
+
+    static Stream<Collection<Integer>> collectionsOfIntFactory() {
+        return Stream.of(
+                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9),
+                Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        );
+    }
+}
