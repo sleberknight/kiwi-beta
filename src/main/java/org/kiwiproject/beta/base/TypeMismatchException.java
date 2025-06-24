a408185af5fb8c12360fd0ecb58be66a45f5db17
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-06-23 21:51:56 -0400

    Refactor TypeMismatchException
    * Add expectedType and actualType
    * Remove standard constructors
    * Add new factory method that permits custom messages

--- src/main/java/org/kiwiproject/beta/base/TypeMismatchException.java
+++ src/main/java/org/kiwiproject/beta/base/TypeMismatchException.java
@@ -1,5 +1,6 @@
 package org.kiwiproject.beta.base;
 
+import static org.kiwiproject.base.KiwiPreconditions.requireNotNull;
 import static org.kiwiproject.base.KiwiStrings.f;
 
 import com.google.common.annotations.Beta;
@@ -9,73 +10,52 @@ import com.google.common.annotations.Beta;
  * <p>
  * This exists mainly to provide more information in certain situations
  * than {@link ClassCastException} does. For example, when checking that
- * a List contains only Integer values but a Double is found. Or when
+ * a List contains only Integer values, but a Double is found. Or when
  * checking that a Map contains the expected key and value types.
- * <p>
- * While you can construct an instance using the "standard" constructors,
- * it is preferable to create instances the factory methods which can
- * provide better semantic meaning, and better messages describing the
- * problem.
  */
 @Beta
 public class TypeMismatchException extends RuntimeException {
 
-    /**
-     * Constructs a new instance with no detail message.
-     */
-    public TypeMismatchException() {
-    }
+    private final Class<?> expectedType;
+    private final Class<?> actualType;
 
-    /**
-     * Constructs a new instance with the specified detail message.
-     *
-     * @param message the detail message
-     */
-    public TypeMismatchException(String message) {
-        super(message);
-    }
-
-    /**
-     * Constructs a new instance with the specified detail message and cause.
-     *
-     * @param message the detail message
-     * @param cause   the cause
-     */
-    public TypeMismatchException(String message, Throwable cause) {
+    private TypeMismatchException(String message,
+                                  Class<?> expectedType,
+                                  Class<?> actualType,
+                                  Throwable cause) {
         super(message, cause);
+        this.expectedType = requireNotNull(expectedType, "expectedType must not be null");
+        this.actualType = requireNotNull(actualType, "actualType must not be null");
     }
 
     /**
-     * Constructs a new instance with the specified cause and a detail message
-     * of {@code (cause==null ? null : cause.toString())}.
+     * Gets the expected type of the value that caused the type mismatch.
      *
-     * @param cause the cause
+     * @return the Class object representing the expected type
      */
-    public TypeMismatchException(Throwable cause) {
-        super(cause);
+    public Class<?> expectedType() {
+        return expectedType;
     }
 
     /**
-     * Factory method to create a new instance with a standardized message for a type mismatch.
+     * Gets the actual type of the value that caused the type mismatch.
      *
-     * @param expectedType the expected type of the value
-     * @param cause        the ClassCastException that occurred during the cast attempt
-     * @return a new instance with a descriptive message
+     * @return the Class object representing the actual type
      */
-    public static TypeMismatchException forExpectedTypeWithCause(Class<?> expectedType, ClassCastException cause) {
-        return new TypeMismatchException(f("Cannot cast value to type {}", expectedType.getName()), cause);
+    public Class<?> actualType() {
+        return actualType;
     }
 
     /**
-     * Factory method to create a new instance with a standardized message for a type mismatch.
+     * Factory method to create a new instance with a standardized message for a type mismatch and no cause.
      *
      * @param expectedType the expected type of the value
      * @param actualType   the actual type of the value
      * @return a new instance with a descriptive message
      */
     public static TypeMismatchException forUnexpectedType(Class<?> expectedType, Class<?> actualType) {
-        return new TypeMismatchException(f("Cannot cast value of type {} to type {}",
-                actualType.getName(), expectedType.getName()));
+        var message = f("Cannot cast value of type {} to type {}", actualType.getName(), expectedType.getName());
+        return TypeMismatchException.forUnexpectedTypeWithMessage(message, expectedType, actualType, null);
     }
 
      /**
@@ -89,14 +69,13 @@ public class TypeMismatchException extends RuntimeException {
     public static TypeMismatchException forUnexpectedTypeWithCause(Class<?> expectedType,
                                                                    Class<?> actualType,
                                                                    ClassCastException cause) {
-        var message = f("Cannot cast value of type {} to type {}",
-                actualType.getName(), expectedType.getName());
-        return new TypeMismatchException(message, cause);
+        var message = f("Cannot cast value of type {} to type {}", actualType.getName(), expectedType.getName());
+        return TypeMismatchException.forUnexpectedTypeWithMessage(message, expectedType, actualType, cause);
     }
 
     /**
      * Factory method to create a new instance with a standardized message for a type mismatch
-     * on a collection element.
+     * on a collection element. The returned exception has no cause.
      * 
      * @param collectionType the type of collection, such as Collection, List, or Set
      * @param expectedType   the expected type of the collection elements
@@ -108,11 +87,12 @@ public class TypeMismatchException extends RuntimeException {
                                                                            Class<?> actualType) {
         var message = f("Expected {} to contain elements of type {}, but found element of type {}",
                 collectionType.getName(), expectedType.getName(), actualType.getName());
-        return new TypeMismatchException(message);
+        return TypeMismatchException.forUnexpectedTypeWithMessage(message, expectedType, actualType, null);
     }
 
     /**
      * Factory method to create a new instance with a standardized message for a map key type mismatch.
+     * The returned exception has no cause.
      * 
      * @param expectedKeyType the expected type of keys in the map
      * @param actualKeyType   the actual type of key found in the map
@@ -122,11 +102,12 @@ public class TypeMismatchException extends RuntimeException {
                                                                 Class<?> actualKeyType) {
         var message = f("Expected Map to contain keys of type {}, but found key of type {}",
                 expectedKeyType.getName(), actualKeyType.getName());
-        return new TypeMismatchException(message);
+        return TypeMismatchException.forUnexpectedTypeWithMessage(message, expectedKeyType, actualKeyType, null);
     }
 
     /**
      * Factory method to create a new instance with a standardized message for a map value type mismatch.
+     * The returned exception has no cause.
      * 
      * @param expectedValueType the expected type of values in the map
      * @param actualValueType   the actual type of value found in the map
@@ -136,6 +117,23 @@ public class TypeMismatchException extends RuntimeException {
                                                                   Class<?> actualValueType) {
         var message = f("Expected Map to contain values of type {}, but found value of type {}",
                 expectedValueType.getName(), actualValueType.getName());
-        return new TypeMismatchException(message);
+        return TypeMismatchException.forUnexpectedTypeWithMessage(message, expectedValueType, actualValueType, null);
+    }
+
+    /**
+     * Factory method to create a new instance of {@code TypeMismatchException} with a custom message,
+     * expected type, actual type, and an optional cause.
+     *
+     * @param message      a custom descriptive message explaining the type mismatch
+     * @param expectedType the expected type of the value
+     * @param actualType   the actual type of the value
+     * @param cause        the (optional) {@code ClassCastException} that occurred during the cast attempt
+     * @return a new instance of {@code TypeMismatchException} representing the type mismatch
+     */
+    public static TypeMismatchException forUnexpectedTypeWithMessage(String message,
+                                                                     Class<?> expectedType,
+                                                                     Class<?> actualType,
+                                                                     ClassCastException cause) {
+        return new TypeMismatchException(message, expectedType, actualType, cause);
     }
 }
--- src/test/java/org/kiwiproject/beta/base/TypeMismatchExceptionTest.java
+++ src/test/java/org/kiwiproject/beta/base/TypeMismatchExceptionTest.java
@@ -2,51 +2,35 @@ package org.kiwiproject.beta.base;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.kiwiproject.test.junit.jupiter.StandardExceptionTests.standardConstructorTestsFor;
 
-import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
 
-import java.util.Collection;
 import java.util.Set;
 
 class TypeMismatchExceptionTest {
-
-    @TestFactory
-    Collection<DynamicTest> shouldHaveStandardConstructors() {
-        return standardConstructorTestsFor(TypeMismatchException.class);
-    }
-
-    @Test
-    void shouldCreate_forExpectedTypeWithCause() {
-        var cause = new ClassCastException();
-        var ex = TypeMismatchException.forExpectedTypeWithCause(Double.class, cause);
-
-        assertAll(
-            () -> assertThat(ex).hasMessage("Cannot cast value to type java.lang.Double"),
-            () -> assertThat(ex).hasCause(cause)
-        );
-    }
 
     @Test
     void shouldCreate_forTypeMismatch_WithValueType_AndUnexpectedType() {
         var ex = TypeMismatchException.forUnexpectedType(Double.class, Integer.class);
 
-         assertAll(
-            () -> assertThat(ex).hasMessage("Cannot cast value of type java.lang.Integer to type java.lang.Double"),
-            () -> assertThat(ex).hasNoCause()
+        assertAll(
+                () -> assertThat(ex).hasMessage("Cannot cast value of type java.lang.Integer to type java.lang.Double"),
+                () -> assertThat(ex).hasNoCause(),
+                () -> assertThat(ex.expectedType()).isEqualTo(Double.class),
+                () -> assertThat(ex.actualType()).isEqualTo(Integer.class)
         );
     }
 
     @Test
-    void shouldCreate_forTypeMismatch_WithValueType_AndUnexpectedTypeh_AndClassCastExceptionAsCause() {
+    void shouldCreate_forTypeMismatch_WithValueType_AndUnexpectedType_AndClassCastExceptionAsCause() {
         var cause = new ClassCastException();
-        var ex = TypeMismatchException.forUnexpectedTypeWithCause(Double.class, Integer.class, cause);
+        var ex = TypeMismatchException.forUnexpectedTypeWithCause(String.class, Integer.class, cause);
 
-         assertAll(
-            () -> assertThat(ex).hasMessage("Cannot cast value of type java.lang.Integer to type java.lang.Double"),
-            () -> assertThat(ex).hasCause(cause)
+        assertAll(
+                () -> assertThat(ex).hasMessage("Cannot cast value of type java.lang.Integer to type java.lang.String"),
+                () -> assertThat(ex).hasCause(cause),
+                () -> assertThat(ex.expectedType()).isEqualTo(String.class),
+                () -> assertThat(ex.actualType()).isEqualTo(Integer.class)
         );
     }
 
@@ -55,8 +39,10 @@ class TypeMismatchExceptionTest {
         var ex = TypeMismatchException.forUnexpectedCollectionElementType(Set.class, Integer.class, Double.class);
 
         assertAll(
-            () -> assertThat(ex).hasMessage("Expected java.util.Set to contain elements of type java.lang.Integer, but found element of type java.lang.Double"),
-            () -> assertThat(ex).hasNoCause()
+                () -> assertThat(ex).hasMessage("Expected java.util.Set to contain elements of type java.lang.Integer, but found element of type java.lang.Double"),
+                () -> assertThat(ex).hasNoCause(),
+                () -> assertThat(ex.expectedType()).isEqualTo(Integer.class),
+                () -> assertThat(ex.actualType()).isEqualTo(Double.class)
         );
     }
 
@@ -65,8 +51,10 @@ class TypeMismatchExceptionTest {
         var ex = TypeMismatchException.forUnexpectedMapKeyType(String.class, Integer.class);
 
         assertAll(
-            () -> assertThat(ex).hasMessage("Expected Map to contain keys of type java.lang.String, but found key of type java.lang.Integer"),
-            () -> assertThat(ex).hasNoCause()
+                () -> assertThat(ex).hasMessage("Expected Map to contain keys of type java.lang.String, but found key of type java.lang.Integer"),
+                () -> assertThat(ex).hasNoCause(),
+                () -> assertThat(ex.expectedType()).isEqualTo(String.class),
+                () -> assertThat(ex.actualType()).isEqualTo(Integer.class)
         );
     }
 
@@ -75,8 +63,10 @@ class TypeMismatchExceptionTest {
         var ex = TypeMismatchException.forUnexpectedMapValueType(Integer.class, Long.class);
 
         assertAll(
-            () -> assertThat(ex).hasMessage("Expected Map to contain values of type java.lang.Integer, but found value of type java.lang.Long"),
-            () -> assertThat(ex).hasNoCause()
+                () -> assertThat(ex).hasMessage("Expected Map to contain values of type java.lang.Integer, but found value of type java.lang.Long"),
+                () -> assertThat(ex).hasNoCause(),
+                () -> assertThat(ex.expectedType()).isEqualTo(Integer.class),
+                () -> assertThat(ex.actualType()).isEqualTo(Long.class)
         );
     }
 }
