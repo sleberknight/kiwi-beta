3dda1e40933d0025897eaf89aac1d1cb6343f131
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2026-04-30 19:29:48 -0400

    feat: deprecate KiwiSlf4j log methods for removal (#615)
    Deprecates the KiwiSlf4j#log methods in favor of SLF4J 2's fluent
    logging API.
    
    The class remains useful for isEnabled helpers and case-insensitive
    conversion of string level names to SLF4J Level values.
    
    Also adds minimal deprecation Javadocs and Kotlin ReplaceWith
    suggestions for the extension functions.
    
    Document the remaining methods that are not deprecated and which do not
    already have Javadoc.
    
    Closes #613

--- src/main/kotlin/org/kiwiproject/beta/slf4j/KiwiSlf4jExtensions.kt
+++ src/main/kotlin/org/kiwiproject/beta/slf4j/KiwiSlf4jExtensions.kt
@@ -5,23 +5,47 @@ import org.slf4j.event.Level
 
 fun Logger.isEnabled(level: Level): Boolean = KiwiSlf4j.isEnabled(this, level)
 
+@Deprecated(
+    message = "Use SLF4J's fluent logging API: logger.atLevel(level).log(message)",
+    replaceWith = ReplaceWith("this.atLevel(level).log(message)")
+)
+@Suppress("DEPRECATION", "removal", "kotlin:S1874", "kotlin:S1133")
 fun Logger.log(level: Level, message: String) = KiwiSlf4j.log(this, level, message)
 
+@Deprecated(
+    message = "Use SLF4J's fluent logging API: logger.atLevel(level).log(format, arg)",
+    replaceWith = ReplaceWith("this.atLevel(level).log(format, arg)")
+)
+@Suppress("DEPRECATION", "removal", "kotlin:S1874", "kotlin:S1133")
 fun Logger.log(level: Level, format: String, arg: Any) =
     KiwiSlf4j.log(this, level, format, arg)
 
+@Deprecated(
+    message = "Use SLF4J's fluent logging API: logger.atLevel(level).log(format, arg1, arg2)",
+    replaceWith = ReplaceWith("this.atLevel(level).log(format, arg1, arg2)")
+)
+@Suppress("DEPRECATION", "removal", "kotlin:S1874", "kotlin:S1133")
 fun Logger.log(level: Level, format: String, arg1: Any, arg2: Any) =
     KiwiSlf4j.log(this, level, format, arg1, arg2)
 
 // Add guard around log to avoid an array copy by the spread operator unless actually necessary.
 // I don't know how else to "fix" the use of the spread operator here.
 // See https://detekt.github.io/detekt/performance.html#spreadoperator for more information
-@Suppress("SpreadOperator")
+@Deprecated(
+    message = "Use SLF4J's fluent logging API: logger.atLevel(level).log(format, *arguments)",
+    replaceWith = ReplaceWith("this.atLevel(level).log(format, *arguments)")
+)
+@Suppress("DEPRECATION", "removal", "SpreadOperator", "kotlin:S1874", "kotlin:S1133")
 fun Logger.log(level: Level, format: String, vararg arguments: Any) {
     if (isEnabled(level)) {
         KiwiSlf4j.log(this, level, format, *arguments)
     }
 }
 
+@Deprecated(
+    message = "Use SLF4J's fluent logging API: logger.atLevel(level).log(message, t)",
+    replaceWith = ReplaceWith("this.atLevel(level).setCause(t).log(message)")
+)
+@Suppress("DEPRECATION", "removal", "kotlin:S1874", "kotlin:S1133")
 fun Logger.log(level: Level, message: String, t: Throwable) =
     KiwiSlf4j.log(this, level, message, t)
